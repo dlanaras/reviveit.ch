d61fb83334dd62832f812121c63f64b2441fbd91
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-10-27 21:09:54 +0200

    fix: fixed footer to stay on bottom

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -2,17 +2,18 @@
     <h4><a href="https://reviveit.ch">reviveit.ch</a></h4>
     <h4>ReviveIT Team</h4>
     <h4>🄯 GPLv3 {new Date().getFullYear()}</h4>
-  </div>
-  
-  <style>
-    div {
-        background-color: #800080;
-      display: flex;
-      justify-content: space-around;
-      width: 100%;
-      height: 7%;
-      overflow:hidden;
-      align-items: center;
-      min-height: 3rem;
-    }
-  </style>
+</div>
+
+<style>
+  div {
+    background-color: #800080;
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    overflow:hidden;
+    align-items: center;
+    height: 5rem;
+    position: fixed;
+    bottom: 0;
+  }
+</style>
