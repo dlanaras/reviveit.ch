836c4dcd74a08675bc0f4353d1d356692988e1a3
Nils Hindermann <62908318+nilshindermann@users.noreply.github.com>
2023-11-22 21:09:24 +0100

    don't use headers in Footer

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -1,19 +1,19 @@
 <div id="footer">
-    <h4><a href="https://reviveit.ch">reviveit.ch</a></h4>
-    <h4>ReviveIT Team</h4>
-    <h4>🄯 GPLv3 {new Date().getFullYear()}</h4>
+	<p><a href="https://reviveit.ch">reviveit.ch</a></p>
+	<p>ReviveIT Team</p>
+	<p>🄯 GPLv3 {new Date().getFullYear()}</p>
 </div>
 
 <style>
-  div {
-    background-color: #800080;
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
-    overflow:hidden;
-    align-items: center;
-    height: 5rem;
-    position: fixed;
-    bottom: 0;
-  }
+	div {
+		background-color: #800080;
+		display: flex;
+		justify-content: space-around;
+		width: 100%;
+		overflow: hidden;
+		align-items: center;
+		height: 5rem;
+		position: fixed;
+		bottom: 0;
+	}
 </style>
