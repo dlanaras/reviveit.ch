68380f8b9d66b49133f1835fbc6c0b1058dcda21
Nils Hindermann <62908318+nilshindermann@users.noreply.github.com>
2023-11-22 22:55:29 +0100

    navigation padding

--- frontend/src/components/navigation.element.svelte
+++ frontend/src/components/navigation.element.svelte
@@ -1,48 +1,48 @@
 <script>
-  import { page } from "$app/stores";
+	import { page } from '$app/stores';
 
-  export let path = "/";
-  export let name = "m152";
+	export let path = '/';
+	export let name = 'm152';
 </script>
 
-
-<a href={path} class={$page.url.pathname === path ? "active" : ""}>
-  {#if (path !== "/")}
-    {name}
-  {:else}
-    <img src="reviveit.logo.png" alt="ReviveIT logo" height="96px">
-  {/if}
+<a href={path} class={$page.url.pathname === path ? 'active' : ''}>
+	{#if path !== '/'}
+		{name}
+	{:else}
+		<img src="reviveit.logo.png" alt="ReviveIT logo" height="96px" />
+	{/if}
 </a>
 
 <style>
-  a:hover {
-    background-color: #800080;
-  }
+	a:hover {
+		background-color: #800080;
+	}
 
-  a:link {
-    text-decoration: inherit;
-    color: inherit;
-  }
+	a:link {
+		text-decoration: inherit;
+		color: inherit;
+	}
 
-  a:visited {
-    text-decoration: inherit;
-    color: inherit;
-  }
+	a:visited {
+		text-decoration: inherit;
+		color: inherit;
+	}
 
-  a {
-    width: 100%;
-    font-size: 1.5em;
-    font-weight: bold;
-  }
+	a {
+		width: 100%;
+		font-size: 1.5em;
+		font-weight: bold;
+		padding: 0.7em 0;
+	}
 
-  .active {
-    background-color: #800080;
-    cursor: pointer;
-  }
+	.active {
+		background-color: #800080;
+		cursor: pointer;
+	}
 
-  img {
-    margin-top: 16px;
-    margin-bottom: 16px;
-    filter: drop-shadow(3px 3px 0 black);
-  }
+	img {
+		margin-top: 16px;
+		margin-bottom: 16px;
+		filter: drop-shadow(3px 3px 0 black);
+	}
 </style>
