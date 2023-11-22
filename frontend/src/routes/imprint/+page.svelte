536531b276e1518266a39033b8d8a37be228cf5e
Nils Hindermann <62908318+nilshindermann@users.noreply.github.com>
2023-11-22 22:43:43 +0100

    add imprint and privacy-policy

---
+++ frontend/src/routes/imprint/+page.svelte
@@ -0,0 +1,38 @@
+<div>
+	<p>
+		<b>Verantwortliche Instanz:</b><br />
+		ReviveIT<br />Austellungsstrasse 70<br />
+		Zürich 8005<br />
+		Schweiz<br />
+		<strong>E-Mail</strong>: info@reviveit.ch<br /><br />
+		<strong>Vertretungsberechtigte Personen</strong><br />
+		Sven Kevin Oppliger<br />
+		Dimitrios Lanaras<br />
+		Nils Oliver Hindermann<br />
+		Luna Neff<br /><br />
+		<strong>Name des Unternehmens</strong>: ReviveIT<br /><br />
+		<strong>Haftungsausschluss</strong><br />
+		Der Autor übernimmt keine Gewähr für die Richtigkeit, Genauigkeit, Aktualität, Zuverlässigkeit und
+		Vollständigkeit der Informationen.<br />
+		Haftungsansprüche gegen den Autor wegen Schäden materieller oder immaterieller Art, die aus dem Zugriff
+		oder der Nutzung bzw. Nichtnutzung der veröffentlichten Informationen, durch Missbrauch der Verbindung
+		oder durch technische Störungen entstanden sind, werden ausgeschlossen.<br /><br />
+		Alle Angebote sind freibleibend. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder
+		das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung
+		zeitweise oder endgültig einzustellen.<br /><br />
+		<strong>Haftungsausschluss für Inhalte und Links</strong>
+		<br />
+		Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs. Es wird
+		jegliche Verantwortung für solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher Webseiten
+		erfolgen auf eigene Gefahr des jeweiligen Nutzers.<br /><br />
+		<strong>Urheberrechtserklärung</strong><br />
+		Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser
+		Website, gehören ausschliesslich ReviveIT oder den speziell genannten Rechteinhabern. Für die Reproduktion
+		jeglicher Elemente ist die schriftliche Zustimmung des Urheberrechtsträgers im Voraus einzuholen.<br
+		/><br />
+		<strong>Quelle</strong>:
+		<a style="color:inherit;text-decoration:none;" href="https://brainbox.swiss/" target="_blank">
+			BrainBox Solutions
+		</a>
+	</p>
+</div>
