4b0b8eec4ad44195fceb285f4db4921ab947c8c6
Nils Hindermann <62908318+nilshindermann@users.noreply.github.com>
2023-11-23 12:30:56 +0100

    footer links

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -1,19 +1,35 @@
 <div id="footer">
-	<p><a href="https://reviveit.ch">reviveit.ch</a></p>
-	<p>ReviveIT Team</p>
+	<p><a href="https://reviveit.ch" class="link">reviveit.ch</a></p>
+	<p>
+		ReviveIT Team<br />
+		<a href="/imprint">Impressum</a>&nbsp;&middot;&nbsp;<a href="/privacy-policy"
+			>Datenschutzerklärung</a
+		>
+	</p>
 	<p>🄯 GPLv3 {new Date().getFullYear()}</p>
 </div>
 
 <style>
 	div {
-		background-color: #800080;
+		background-color: #614880;
 		display: flex;
 		justify-content: space-around;
 		width: 100%;
 		overflow: hidden;
 		align-items: center;
-		height: 5rem;
 		position: fixed;
 		bottom: 0;
+	}
+
+	p {
+		text-align: center;
+	}
+
+	a {
+		text-decoration: underline;
+	}
+
+	a.link {
+		text-decoration: none !important;
 	}
 </style>
