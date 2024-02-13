5d2ef56ea258422918b8333232fa451815aa9f4c
dlanaras <70207362+dlanaras@users.noreply.github.com>
2024-02-13 22:46:49 +0100

    refactor + hopefully fix

--- frontend/src/routes/admin/+page.server.js
+++ frontend/src/routes/admin/+page.server.js
@@ -8,15 +8,7 @@ export const actions = {
             password: data.get('password')?.toString()
         }
 		await fetch(`${config.apiUrl}/auth`, {method: 'POST', body: JSON.stringify(body)}).then(res => {
-            const authCookie = res.headers.get('cookie');
-            console.log(authCookie, res.headers);
-            cookies.set('auth', getCookieValue('auth', authCookie), {
-                httpOnly: true,
-                secure: true,
-                sameSite: "none",
-                expires: new Date(getCookieValue('Expires', authCookie)),
-                path: getCookieValue('Path', authCookie)
-            })
+            setAuthCookie(res, cookies);
         })
 	},
     register: async ({ request, cookies, fetch }) => {
@@ -27,18 +19,24 @@ export const actions = {
             create_key: data.get('register_secret')?.toString()
         }
 		await fetch(`${config.apiUrl}/create_admin`, {method: 'POST', body: JSON.stringify(body)}).then(res => {
-            const authCookie = res.headers.get('cookie');
-            console.log(authCookie, res.headers);
-            cookies.set('auth', getCookieValue('auth', authCookie), {
-                httpOnly: true,
-                secure: true,
-                sameSite: "none",
-                expires: new Date(getCookieValue('Expires', authCookie)),
-                path: getCookieValue('Path', authCookie)
-            })
+            setAuthCookie(res, cookies);
         })
 	}
 };
+
+//@ts-ignore
+function setAuthCookie(res, cookies) {
+    const authCookie = res.headers.get('set-cookie');
+    console.log(authCookie, res.headers);
+    cookies.set('auth', getCookieValue('auth', authCookie), {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        expires: new Date(getCookieValue('Expires', authCookie)),
+        path: getCookieValue('Path', authCookie)
+    });
+}
+
 //@ts-ignore
 function getCookieValue(name, cookie) 
 {
