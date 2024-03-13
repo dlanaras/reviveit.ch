d3d9de17407a4ac9ef4b431d92fab40e413689fd
dlanaras <70207362+dlanaras@users.noreply.github.com>
2024-03-13 19:51:03 +0100

    fix: usable design now

--- frontend/src/routes/admin/articles/+page.svelte
+++ frontend/src/routes/admin/articles/+page.svelte
@@ -1,11 +1,22 @@
 <div class="content">
-    <form method="POST">
+    <form method="POST" class="column">
         <label for="title">Titel</label>
         <input type="text" name="title" autocomplete="off">
         <label for="content">Inhalt</label>
         <input type="text" name="content" autocomplete="off">
-        <label for="date">Titel</label>
+        <label for="date">Datum</label>
         <input type="date" name="date">
         <input type="submit" value="Erstellen">
     </form>
 </div>
+
+<style>
+    .column {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .column input {
+        color: black;
+    }
+</style>
