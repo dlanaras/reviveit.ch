b98a8dd2bbacf2dee81a116c3f2585bce8d66268
dlanaras <70207362+dlanaras@users.noreply.github.com>
2024-05-09 13:57:06 +0200

    feat: add social media icons

--- frontend/src/components/footer.svelte
+++ frontend/src/components/footer.svelte
@@ -1,10 +1,15 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</svelte:head>
+
 <div id="footer">
 	<p><a href="https://reviveit.ch" class="link">reviveit.ch</a></p>
 	<p>
 		ReviveIT Team<br />
 		<a href="/imprint">Impressum</a>&nbsp;&middot;&nbsp;<a href="/privacy-policy"
 			>Datenschutzerklärung</a
-		>
+		>&nbsp;&middot;&nbsp;<a href="" target="_blank" class="fa fa-linkedin"/>&nbsp;&middot;&nbsp;
+		<a href="https://www.youtube.com/channel/UCVoNLK0OEj2DMcECFFJ4yUA/about" target="_blank" class="fa fa-youtube" />
 	</p>
 	<p>
 		<span style="rotate: 180deg; display: inline-block">&copy;</span> GPLv3 {new Date().getFullYear()}
@@ -34,6 +39,10 @@
 
 	a.link {
 		text-decoration: none !important;
+	}
+
+	.fa {
+		text-decoration: none;
 	}
 
 	@media screen and (min-width: 600px) {
